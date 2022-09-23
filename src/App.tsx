3a76fb4d57af4b66651b0de186fc3fb1e6b8a6fb
ghostangel77 <ghostangel77@gmail.com>
2022-09-23 10:46:35 -0500

    Updated the style of the form.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TaskForm } from "./components/taskForm/taskForm.component";
 function App() {
   return (
     <main className="h-screen bg-zinc-900">
-      <div className="container mx-auto p-10">
+      <div className="container mx-auto p-10 pt-0">
         <TaskForm />
         <Tasks />
       </div>
